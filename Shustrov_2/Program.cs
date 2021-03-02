e1965d85896fe8ce8408ce2838cd23cf2b99f9fb
Bird Egop <sampletext32@bk.ru>
2021-03-02 22:26:40 +0300

    Add project

---
+++ Shustrov_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Shustrov_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float m = float.Parse(Console.ReadLine());
+            float n = float.Parse(Console.ReadLine());
+            float c = float.Parse(Console.ReadLine());
+
+            float b;
+            if (m > 0)
+            {
+                b = m * m;
+            }
+            else if (m == 0)
+            {
+                b = n;
+            }
+            else
+            {
+                b = (float)(m * Math.Cos(m));
+            }
+
+
+            float a;
+            if (b!=c)
+            {
+                a = b * b;
+            }
+            else
+            {
+                a = (float)(b * Math.Cos(b));
+            }
+
+
+            float W = (float)((a * a + Math.Cos(a)) / (a + Math.Sin(a)* Math.Sin(a)* Math.Sin(a)) - Math.Tan(a));
+
+            Console.WriteLine(W);
+
+        }
+    }
+}
